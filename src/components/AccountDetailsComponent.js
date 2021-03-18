f3319ed33c79fddf074f2239a50fadee41145180
aduterte <alexduterte@gmail.com>
2021-03-18 15:06:55 -0400

    account settings updated

--- src/components/AccountDetailsComponent.js
+++ src/components/AccountDetailsComponent.js
@@ -1,26 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useRecoilValue} from 'recoil'
 import {userState} from "../atoms"
+
+
 export default function AccountSettingsComponent(){
 
-    const user = useRecoilValue(userState)
+    const user = useRecoilValue(userState),
+        [edit, setEdit] = useState(false)
 
     return (
         <div className="account-details">
             <div className="account-details-top">
                 <div>
-                    <img src={user.photo} alt="user picture" className="medium-avatar"/>
+                    <img src={user.photo} alt="user avatar" className="medium-avatar"/>
                 </div>
                 <div className="account-details-username">
                     {user.username}
                 </div>
                 
             </div>
+            <div onClick={()=>{setEdit(!edit)}}>Edit Profile</div>
             <div className="h-line"/>
-            <div>{user.first_name}</div>
-            <div>{user.email}</div>
-            <div>{user.last_name}</div>
-            <div>{user.zip_code}</div>
+            {edit ?
+            <form>
+                <div>Your email: form{user.email}</div>
+                <div>First Name: {user.first_name}</div>
+                <div>Last Name: {user.last_name}</div>
+                <div>Zip Code: {user.zip_code}</div>
+                <input type="submit"></input>
+            </form> :
+            <div>
+                <div>Your email: {user.email}</div>
+            <div>First Name: {user.first_name}</div>
+            <div>Last Name: {user.last_name}</div>
+            <div>Zip Code: {user.zip_code}</div>
+            </div>
+            }
+            
             
             
         </div>
